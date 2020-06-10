96e3b706d261c43b26a08648e4fb636ab30d7610
Ashwin-Ramesh2607 <ashwin.cuber26@gmail.com>
2020-06-10 09:40:23 +0530

    incorporate confidence threshold

--- image_demo.py
+++ image_demo.py
@@ -1,33 +1,65 @@
 import argparse
 
 import cv2
+import numpy as np
 
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 
 
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(description='Social Distancing AI')
+
+    parser.add_argument(
+        '--image_path',
+        type=str,
+        default='',
+        help='Path to the demo image file')
+
+    parser.add_argument(
+        '--config_path',
+        type=str,
+        default='configs/iterdet/crowd_human_full_faster_rcnn_r50_fpn_2x.py',
+        help='Path to the config file for the required model')
+
+    parser.add_argument(
+        '--model_path',
+        type=str,
+        default='crowd_human_full_faster_rcnn_r50_fpn_2x.pth',
+        help='Path to the trained model for inference')
+
+    parser.add_argument(
+        '--device',
+        type=str,
+        default='cuda:0',
+        help='Device name and number to be used for inference')
+
+    parser.add_argument(
+        '--score_thresh',
+        type=float,
+        default=0.3,
+        help='Score threshold above which to display bounding boxes')
+
+    return parser.parse_args()
+
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('img', help='Image file')
-    parser.add_argument('config', help='Config file')
-    parser.add_argument('checkpoint', help='Checkpoint file')
-    parser.add_argument(
-        '--device', default='cuda:0', help='Device used for inference')
-    parser.add_argument(
-        '--score-thr', type=float, default=0.3, help='bbox score threshold')
-    args = parser.parse_args()
 
     # build the model from a config file and a checkpoint file
-    model = init_detector(args.config, args.checkpoint, device=args.device)
+    model = init_detector(FLAGS.config_path, FLAGS.model_path, device=FLAGS.device)
 
     # test a single image
-    img = cv2.imread(args.img)
-    result = inference_detector(model, img)[0]
-    result = result.astype(np.int16)
+    image = cv2.imread(FLAGS.image_path)
+    result = inference_detector(model, image)[0]
+
     # show the results
     for box in result:
-        cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0, 255, 255), 2)
-    cv2.write('test.jpg', img)
+        if box[4] >= FLAGS.score_thresh:
+            cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 1)
+
+    cv2.imwrite('test.jpg', image)
 
 
 if __name__ == '__main__':
+    FLAGS = parse_arguments()
     main()
